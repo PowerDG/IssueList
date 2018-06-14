65ec37d21327e8a11b8b811416821419515e68e9
yuchen1030 <yuchen1030@163.com>
2018-06-14 20:05:51 +0800

    添加项目文件。

---
+++ IssueList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" record some question with picture.");
+            Console.WriteLine("1. ocelot cache issue.");
+            Console.ReadLine();
+        }
+    }
+}
